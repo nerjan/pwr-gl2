aba7a55ece07c3613443eea9fe9cbe17abb08d8e
Borys Szefczyk <boryszef@gmail.com>
2018-07-22 09:46:49 +0200

    test if bootstrap is properly set up

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
-from flask import Flask
 from flask_testing import LiveServerTestCase
 from urllib.request import urlopen
+from selenium import webdriver
+# Selenium requires drivers to be downloaded manually.
+# For Firefox, the geckodriver which can be found here:
+# https://github.com/mozilla/geckodriver/releases/
+# Note that the version of the driver must be compatible
+# with the installed version of Firefox
 import unittest
 import os
 import app
@@ -21,6 +26,29 @@ class TestServer(LiveServerTestCase):
         self.assertIn('GENOMELINK_CLIENT_ID', os.environ)
         self.assertIn('GENOMELINK_CLIENT_SECRET', os.environ)
         self.assertIn('GENOMELINK_CALLBACK_URL', os.environ)
+
+    def test_has_bootstrap(self):
+        driver = webdriver.Firefox()
+        driver.get(self.get_server_url())
+
+        # Check if <link ...bootstrap...> found
+        css = driver.find_element_by_xpath('/html/head/link')
+        self.assertIn('bootstrap', css.get_attribute('href'))
+
+        # Check that JS for bootstrap, jquery and popper are loaded
+        look_for = ['bootstrap' , 'jquery', 'popper']
+        scripts = driver.find_elements_by_xpath('/html/body/script')
+        for s in scripts:
+            src = s.get_attribute('src')
+            print(src)
+            for tag in look_for:
+                print(tag, look_for)
+                if tag in src:
+                    look_for.remove(tag)
+                    break
+        self.assertListEqual(look_for, [])
+
+        driver.close()
 
 
 if __name__ == '__main__':
