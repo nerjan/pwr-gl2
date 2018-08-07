8d2331decb98aad24a8072f225ef58f3727199d4
nerjan <jan.belkner@gmail.com>
2018-08-07 14:51:06 +0200

    1. LoginForm class used in login.html

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ from flask_menu import Menu, register_menu
 import genomelink
 import configparser
 import os
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from flask_login import login_user,logout_user, login_manager, LoginManager, current_user
+from db_declarative import User , loadSession
+from functools import wraps
 
 app = Flask(__name__)
 Menu(app=app)
-
+login = LoginManager(app)
 
 traits = ('agreeableness', 'conscientiousness', 'extraversion', 'neuroticism',
           'openness')
@@ -21,17 +25,63 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/login")
-def login():
-    return render_template('index.html')
 
+class LoginForm(FlaskForm):
+    '''Form that allows to login. At the end of the project all forms could be in separated file'''
+    username = StringField('Username')#, validators=[DataRequired()]) #I dont use it to make message "Wrong password or username" visible
+    password = PasswordField('Password')
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+@login.user_loader
+def load_user(id):
+    '''This callback is used to reload the user object from the user ID stored in the session'''
+    session = loadSession()
+    return session.query(User).get(int(id))
+
+@app.route("/login", methods=['GET', 'POST'])
+@register_menu(app, '.login','Sing in',order=4)
+def login():
+    form = LoginForm()
+    if current_user.is_authenticated:
+        flash("You are already logged in")
+        return redirect(url_for('index'))
+    if form.validate_on_submit():
+        '''check if username is in database- search to first username,
+        because I assume there is no 2 users with the same username!!!'''
+        session =loadSession()
+        user = session.query(User).filter_by(username=form.username.data).first()
+        if user and form.password.data==user.password: #if username and password is ok login
+            flash('Logged in successfully as {}'.format(form.username.data))
+            login_user(user, remember=form.remember_me.data)
+            return render_template('index.html')
+        else:
+            flash("Wrong password or username")
+            return redirect(url_for('login'))
+    return render_template('login.html', title='Sing In ',
+                           form=form)
+
+def login_required(f):
+    '''is used to redirec if one want to get to page that should be allowed only for logged in user'''
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if current_user.is_authenticated:
+            return f(*args, **kwargs)
+        else:
+            flash("You need to login first")
+            return redirect(url_for('login'))
+
+    return wrap
 
 @app.route("/logout")
+@login_required #if not logged in dont allowd to do
+@register_menu(app, '.logout','Sing out',order=5) #HERE HOW TO DO TO SHOW ONLY SING IN OR SING OUT??!!
 def logout():
-    return render_template('index.html')
-
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route("/genome")
+@login_required #if not logged in dont allowd to do
 @register_menu(app, '.genome', 'Genomic insight', order=1)
 def genome():
     '''Display genomic insight based on GenomeLink API'''
@@ -52,6 +102,7 @@ def genome():
 
 
 @app.route("/questionare")
+@login_required #if not logged in dont allowd to do
 @register_menu(app, '.questionare', 'Self-assessment questionare', order=2)
 def questionare():
     '''Show self-assessment questionare'''
@@ -59,6 +110,7 @@ def questionare():
 
 
 @app.route("/selfassessment")
+@login_required #if not logged in dont allowd to do) 
 @register_menu(app, '.selfassessment', 'Self-assessment results', order=3)
 def selfassessment():
     '''Show self-assessment results'''
