d768cb7b0cc95bdb10d447a67f89af9c34c7e6f7
Borys Szefczyk <boryszef@gmail.com>
2018-08-15 22:16:20 +0200

    test models and views

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,6 @@
 from flask_testing import TestCase
 from app import create_app, db, yaml
-from app.models import *
+from app.models import User, Question, GLTrait
 
 
 class ModelTestCase(TestCase):
@@ -35,12 +35,16 @@ class TestUser(ModelTestCase):
              'email': 'bob@dot.com',
              'password': 'bob123'}
 
-    def test_users_add(self):
+    def test_user_add(self):
 
         user = User(**self.USER1)
         db.session.add(user)
         db.session.commit()
         self.assertIn(user, db.session)
+
+        q = User.query.first()
+        self.assertEqual(q.username, self.USER1['username'])
+        self.assertEqual(q.email, self.USER1['email'])
 
     def test_add_three_users(self):
 
@@ -83,3 +87,44 @@ class TestQuestion(ModelTestCase):
         self.assertEqual(quest[0].question, "Lorem ipsum")
         self.assertEqual(quest[0].trait, "latin")
         self.assertEqual(quest[0].impact, "positive")
+
+    def test_add_question(self):
+
+        question = "To be or not to be!"
+        trait = "conscientiousness"
+        impact = "negative"
+
+        q1 = Question(question=question,
+                      trait=trait,
+                      impact=impact)
+        db.session.add(q1)
+        db.session.commit()
+
+        q2 = Question.query.first()
+        self.assertEqual(q2.question, question)
+        self.assertEqual(q2.trait, trait)
+        self.assertEqual(q2.impact, impact)
+
+
+class TestGLTrait(ModelTestCase):
+
+    def test_add_trait_for_user(self):
+
+        mike = User(username='mike', password='123', email='mike@there.net')
+        db.session.add(mike)
+
+        trait = 'openness'
+        description = 'Lorem ipsum'
+        t_score = 0
+        tr = GLTrait(trait=trait, description=description, t_score=t_score,
+                     user=mike)
+        db.session.add(tr)
+        db.session.commit()
+
+        mike_tr = mike.gltrait[0]
+        self.assertIs(mike_tr, tr)
+        tr2 = GLTrait.query.first()
+        self.assertEqual(tr2.user_id, mike.id)
+        self.assertEqual(tr2.trait, trait)
+        self.assertEqual(tr2.description, description)
+        self.assertEqual(tr2.t_score, t_score)
