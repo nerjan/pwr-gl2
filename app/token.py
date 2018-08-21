1b70e9fc9fe4ae15e9083a6986338c5b78da5fb1
nerjan <jan.belkner@gmail.com>
2018-08-21 13:04:09 +0200

    generate unique confirmation html address

--- app/token.py
+++ app/token.py
@@ -1,19 +1,20 @@
 
 from itsdangerous import URLSafeTimedSerializer
-import app.config as config
+import app
+# import app.config as config
 
 
 def generate_confirmation_token(email):
-    serializer = URLSafeTimedSerializer(config.SECRET_KEY)
-    return serializer.dumps(email, salt=config.SECURITY_PASSWORD_SALT)
+    serializer = URLSafeTimedSerializer(app.app.config.get("SECRET_KEY"))
+    return serializer.dumps(email, salt=app.app.config.get("SECURITY_PASSWORD_SALT"))
 
 
-def confirm_token(token, expiration=3600):
-    serializer = URLSafeTimedSerializer(config.SECRET_KEY)
+def confirm_token(token, expiration=86000):
+    serializer = URLSafeTimedSerializer(app.app.config.get("SECRET_KEY"))
     try:
         email = serializer.loads(
             token,
-            salt=config.SECURITY_PASSWORD_SALT,
+            salt=app.app.config.get("SECURITY_PASSWORD_SALT"),
             max_age=expiration
         )
     except:
