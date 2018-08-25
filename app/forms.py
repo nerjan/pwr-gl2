d91b536d7559a462910f6f0abbae599248b0c280
Borys Szefczyk <boryszef@gmail.com>
2018-08-25 21:28:54 +0200

    unify

--- app/forms.py
+++ app/forms.py
@@ -7,32 +7,15 @@ class LoginForm(FlaskForm):
     '''Form that allows to login.'''
 
     username = StringField('Username')
-    name = StringField('Name')
-    surname = StringField('Last name')
     password = PasswordField('Password')
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
-
-
-class SearchForm(FlaskForm):
-    searchfriend = StringField('Search for your friend', [
-        validators.Length(min=4, max=50,
-                          message="Search request has to be between 4 and 50 characters")
-    ])
 
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', [
         validators.Length(min=4, max=20,
                           message="Username has to be between 4 and 20 characters")
-    ])
-    name = StringField('Name', [
-        validators.Length(min=4, max=20,
-                      message="Name has to be between 4 and 20 characters")
-    ])
-    surname = StringField('Last name', [
-        validators.Length(min=4, max=20,
-                          message="Last name has to be between 4 and 20 characters")
     ])
     email = StringField('Email Address', [
         validators.Length(min=6, max=50,
@@ -69,19 +52,6 @@ class ForgottenPasswordForm(FlaskForm):
     email = StringField('Email Address')
     submit = SubmitField('Remind password!')
     forgottenPassoword = SubmitField('I forgot password')
-
-
-class QuestionareForm(FlaskForm):
-    id = HiddenField("Question ID")
-    show_all = BooleanField("Show answered questions too", default=True)
-    answers = RadioField('Label')
-    submit = SubmitField('Submit')
-
-
-class FriendRequest(FlaskForm):
-    id = HiddenField("User ID")
-    submit = SubmitField('Add')
-
 
 
 class SelfAssesmentBarsForm(FlaskForm):
